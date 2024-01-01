f84d70279f3ceacce620746fdce78e5be62d2456
ahsanwebengr <ahsanwebengr@gmail.com>
2024-01-01 22:04:17 +0500

    feat: folder setup

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const user = {
+        _id: '12',
+        role: 'admin',
+    };
+    return (
+        <nav>
+            <Link to={'/'} >Home</Link>
+            <Link to={'/search'} > <FaSearch /> </Link>
+            <Link to={'/cart'} > <FaShoppingBag /> </Link>
+            {user?._id ?
+                <>
+                    <button onClick={() => setIsOpen((prev) => !prev)}> <FaUser /> </button>
+                    <dialog open={isOpen}>
+                        <div>
+                            {user.role === 'admin' && (<Link to={'/admin/dashboard'}>Admin</Link>)}
+                            <Link to={'/orders'} >Orders</Link>
+                            <button> <FaSignOutAlt /></button>
+                        </div>
+                    </dialog>
+                </>
+                : <Link to={'/login'} > <FaSignInAlt /></Link>}
+        </nav>
+    );
+};
+
+export default Header;
