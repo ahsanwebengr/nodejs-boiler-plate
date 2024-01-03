d9c5610981df9d9be6f68b4e847e3494a875e01a
ahsanwebengr <ahsanwebengr@gmail.com>
2024-01-03 21:28:50 +0500

    feat: Cart UI Complete

--- frontend/src/pages/cart/Cart.tsx
+++ frontend/src/pages/cart/Cart.tsx
@@ -1,7 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { VscError } from "react-icons/vsc";
+import CartItem from "../../components/cartItem/CartItem";
+import { Link } from "react-router-dom";
 
-const cartItems = [];
+const cartItems = [
+    {
+        productId: '32439usd',
+        photoUrl: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41GFCpGReNL._AC_UY327_FMwebp_QL65_.jpg',
+        name: 'iPhone',
+        price: 3000,
+        quantity: 40,
+        stock: 10,
+    },
+    {
+        productId: '3233439usd',
+        photoUrl: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41GFCpGReNL._AC_UY327_FMwebp_QL65_.jpg',
+        name: 'iPhone',
+        price: 3000,
+        quantity: 40,
+        stock: 10,
+    }
+];
 const subTotal = 4000;
 const tax = Math.round(subTotal * 0.005);
 const shippingCharge = 200;
@@ -9,17 +28,38 @@ const discount = 100;
 const total = subTotal + shippingCharge + tax;
 
 const Cart = () => {
-    const [couponCode, setCouponCode] = useState<string>('cart');
+    const [couponCode, setCouponCode] = useState<string>('');
     const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            if (Math.random() > 0.5) {
+                setIsValidCouponCode(true);
+            } else {
+                setIsValidCouponCode(false);
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(timeOutId);
+            setIsValidCouponCode(false);
+        };
+    }, [couponCode]);
     return (
         <div className="cart">
-            <main></main>
+            <main>
+                {cartItems.length > 0 ? (
+                    cartItems.map((item) => (
+                        <CartItem key={item.productId} cartItem={item} />
+                    ))
+                ) : <h1>No Items Added.</h1>}
+            </main>
             <aside>
-                <p>Subtotal : $ {subTotal}</p>
-                <p>Shipping Charges : $ {shippingCharge}</p>
+                <p>Subtotal : ${subTotal}</p>
+                <p>Shipping Charges : ${shippingCharge}</p>
                 <p>Tax : $ {tax}</p>
-                <p>Discount : <em>- $ {discount}</em> </p>
-                <strong>Total : $ {total}</strong>
+                <p>Discount : <em className="red">- ${discount}</em> </p>
+                <strong>Total : ${total}</strong>
 
                 <input type="text" placeholder="Coupon Code" value={couponCode} onChange={(e) => setCouponCode(e.target.value)} />
 
@@ -33,6 +73,8 @@ const Cart = () => {
                         )
                     )
                 }
+
+                {cartItems.length > 0 && <Link to={'/shipping'}>Checkout</Link>}
             </aside>
         </div>
     );
