01d3d415f519a365e60b84e1123e80c35107437a
Muhammad Ahsan <muhammad.ahsan@codercrew.xyz>
2024-01-03 11:24:27 +0500

    feat: ProductCart UI

--- frontend/src/pages/cart/Cart.tsx
+++ frontend/src/pages/cart/Cart.tsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
+import { VscError } from "react-icons/vsc";
+
+const cartItems = [];
+const subTotal = 4000;
+const tax = Math.round(subTotal * 0.005);
+const shippingCharge = 200;
+const discount = 100;
+const total = subTotal + shippingCharge + tax;
+
 const Cart = () => {
+    const [couponCode, setCouponCode] = useState<string>('cart');
+    const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
     return (
-        <div>Cart</div>
+        <div className="cart">
+            <main></main>
+            <aside>
+                <p>Subtotal : $ {subTotal}</p>
+                <p>Shipping Charges : $ {shippingCharge}</p>
+                <p>Tax : $ {tax}</p>
+                <p>Discount : <em>- $ {discount}</em> </p>
+                <strong>Total : $ {total}</strong>
+
+                <input type="text" placeholder="Coupon Code" value={couponCode} onChange={(e) => setCouponCode(e.target.value)} />
+
+                {
+                    couponCode && (
+                        isValidCouponCode ? (
+                            <span className="green">$ {discount} off using the <code>{couponCode}
+                            </code></span>
+                        ) : (
+                            <span className="red">Invalid Coupon <VscError /></span>
+                        )
+                    )
+                }
+            </aside>
+        </div>
     );
 };
 
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom"
+import ProductCard from "../../components/productCard/ProductCard"
 
 const Home = () => {
+  const addToCart = () => {
+
+  }
   return (
-    <div>Home</div>
+    <div className="home">
+      <section></section>
+      <div className="heading">
+        <h1>Latest Products</h1>
+        <Link to={'/search'} className="find-more">MORE</Link>
+      </div>
+
+      <main>
+        <ProductCard productId="24fdrtsdgds" name="iPhone" price={500} stock={1} handler={addToCart} photoUrl="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41GFCpGReNL._AC_UY327_FMwebp_QL65_.jpg" />
+      </main>
+
+    </div>
   )
 }
 
