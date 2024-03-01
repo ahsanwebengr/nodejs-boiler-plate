3e2f1bccdc4e0a2e7d25e62a8ce65a6d5e247580
ahsanwebengr <ahsanwebengr@gmail.com>
2024-03-01 17:21:11 +0500

    Added CORS middleware to the Express app

--- germen_resturent/app.js
+++ germen_resturent/app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import auth from "././src/routes/auth.route.js";
 const app = express();
+
+
+app.use(cors());
+app.use(express());
 
 // Route Middlewares 
 app.use("/auth", auth);
