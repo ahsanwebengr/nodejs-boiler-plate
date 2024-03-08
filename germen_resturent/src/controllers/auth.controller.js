07cfbfe0da0dfede0e86f7a7006f721946c08e48
ahsanwebengr <ahsanwebengr@gmail.com>
2024-03-08 10:12:22 +0500

    Added Nodemailer and implemented forgot password functionality.

--- germen_resturent/src/controllers/auth.controller.js
+++ germen_resturent/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/user.model.js";
+import { transporter } from "../senders/emailSend.js";
+import { generateOTP } from "../utils/otp.js";
 
 const createUser = async (req, res) => {
 
@@ -21,7 +23,7 @@ const createUser = async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error", status: false });
     }
 };
 
@@ -32,7 +34,7 @@ const loginUser = async (req, res) => {
         const userLogin = await User.findOne({ email });
 
         if (!userLogin) {
-            return res.status(404).json({ error: "User not found" });
+            return res.status(404).json({ error: "User not found", status: false });
         }
 
         const isPasswordValid = await bcrypt.compare(password, userLogin.password);
@@ -40,21 +42,50 @@ const loginUser = async (req, res) => {
         if (isPasswordValid) {
             res.status(200).json({ message: "User login successfully", userLogin });
         } else {
-            res.status(401).json({ error: "Invalid password" });
+            res.status(401).json({ error: "Invalid password", status: false });
         }
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error", status: false });
     }
 };
 
 const forgotPassword = async (req, res) => {
-try {
-    
-} catch (err) {
-    
-}
+    try {
+        const { email } = req.body;
+        let userEmail = await User.findOne({ email });
+
+        if (!userEmail) {
+            res.status(404).json({ error: 'This email does not exist', status: false });
+        }
+
+        const otp = generateOTP(6);
+
+        const mailOptions = {
+            from: 'jeanne.considine54@ethereal.email',
+            to: email,
+            subject: 'Password Reset',
+            html: `
+        <div style="font-family: 'Arial', sans-serif; background-color: #f4f4f4; padding: 20px; text-align: center;">
+            <h2 style="color: #333;">Password Reset</h2>
+            <p style="color: #555;">Use the following verification code to reset your password:</p>
+            <div style="background-color: #fff; padding: 10px; border: 1px solid #ddd; border-radius: 5px; font-size: 18px; color: #333; margin: 20px 0;">
+                <strong>${otp}</strong>
+            </div>
+            <p style="color: #555;">This code is valid for a limited time. Do not share it with others.</p>
+        </div>
+    `,
+        };
+
+
+        await transporter.sendMail(mailOptions);
+
+        res.status(200).json({ message: 'OTP sent to your email for password reset', status: 'Success' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Server Error', status: false });
+    }
 };
 
 
-export { createUser, loginUser };
+export { createUser, loginUser, forgotPassword };
